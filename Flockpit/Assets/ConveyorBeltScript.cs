0c704180ca8868a0da24d09152fd340a047cd028
Mathew Brown <1700925@uad.ac.uk>
2020-02-03 12:57:52 +0000

    Conveyor Belt Started
    Simple plane with Conveyor Belt script attached to move cube from one end to the other. Code based on example provided at http://popupasylum.co.uk/?p=253 by Mark Hogan.

---
+++ Flockpit/Assets/ConveyorBeltScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Conveyor Belt Code based off code provided Mark Hogan available at popasylum.co.uk 
+//http://popupasylum.co.uk/?p=253
+public class ConveyorBeltScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Rigidbody rigBody = GetComponent<Rigidbody>();
+        rigBody.position -= transform.forward * 2.0f * Time.deltaTime;
+        rigBody.MovePosition(rigBody.position + transform.forward * 2.0f * Time.deltaTime);
+    }
+}
---
+++ Flockpit/Assets/TestRotateScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRotateScript : MonoBehaviour
+{
+    [Range(0.0f, 400.0f)]
+    public float rotateSpeed = 0.0f;
+    public bool manualControl = false;
+    public Vector3 angleVel;
+    Vector3 rotateAxis = new Vector3(0.0f, 1.0f, 0.0f);
+    Rigidbody rigBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        angleVel = rigBody.angularVelocity;
+        if (angleVel.y < 2.5*rotateSpeed)
+        {
+            rigBody.AddTorque(0.0f, 1.0f*rotateSpeed, 0.0f);
+        }
+    }
+}
