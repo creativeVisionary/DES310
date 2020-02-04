34f3e40fe566f7e1e4f10f781a962edc91584f0b
Mathew Brown <1700925@uad.ac.uk>
2020-02-04 16:44:37 +0000

    Item Interaction System Started
    Event OnClickDrag implemented to move objects with mouse click. Event fires but does not operate as intended yet.

---
+++ Flockpit/Assets/Scripts/PlayerScripts/ObjectInteractCursorScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//https://docs.unity3d.com/2018.3/Documentation/ScriptReference/Physics.Raycast.html
+//
+
+public class ObjectInteractCursorScript : MonoBehaviour
+{
+
+    Rigidbody rigBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics.queriesHitTriggers = true;
+        rigBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        //HoldObject();
+    }
+
+    private void OnMouseDrag()
+    {
+        HoldObject();
+    }
+
+    void HoldObject()
+    {
+        Vector3 mouseRay = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        rigBody.velocity = new Vector3(0,0,0);
+        Vector3 oldPos = rigBody.position;
+        oldPos.y = 4.0f;
+        oldPos.x = mouseRay.x;
+        oldPos.z = mouseRay.z;
+        rigBody.position = oldPos;
+    }
+}
