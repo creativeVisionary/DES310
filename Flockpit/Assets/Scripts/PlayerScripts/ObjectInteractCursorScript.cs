b13520b75a68137876aa82a6612cfa6eb53da833
Mathew Brown <1700925@uad.ac.uk>
2020-02-09 21:33:44 +0000

    Progress on Item Interaction
    Items can now be moved across the X and Z Axes. However their position does not entirely match to the cursor. This issue is still being worked on.

--- Flockpit/Assets/Scripts/PlayerScripts/ObjectInteractCursorScript.cs
+++ Flockpit/Assets/Scripts/PlayerScripts/ObjectInteractCursorScript.cs
@@ -9,22 +9,36 @@ public class ObjectInteractCursorScript : MonoBehaviour
 {
 
     Rigidbody rigBody;
+    public Vector3 mousePos;
+    public Vector3 screenMouse;
+    public Vector3 screenObj;
+    public Vector3 difference;
+    public float camDistance;
+
+   
     // Start is called before the first frame update
     void Start()
     {
         Physics.queriesHitTriggers = true;
         rigBody = GetComponent<Rigidbody>();
+        camDistance = 4.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        camDistance = Camera.main.ScreenToWorldPoint(Input.mousePosition).y - rigBody.position.y;
+        Vector3 inputPos = Input.mousePosition;
+        inputPos.z = camDistance;
+        mousePos = Camera.main.ScreenToWorldPoint(inputPos);
+        //
+        screenMouse = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        screenObj = Camera.main.WorldToViewportPoint(rigBody.position);
     }
 
     private void OnMouseDown()
     {
-        //HoldObject();
+
     }
 
     private void OnMouseDrag()
@@ -34,12 +48,17 @@ public class ObjectInteractCursorScript : MonoBehaviour
 
     void HoldObject()
     {
-        Vector3 mouseRay = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        rigBody.velocity = new Vector3(0,0,0);
-        Vector3 oldPos = rigBody.position;
-        oldPos.y = 4.0f;
-        oldPos.x = mouseRay.x;
-        oldPos.z = mouseRay.z;
-        rigBody.position = oldPos;
+        //
+        difference = screenObj - screenMouse;
+        Vector3 inputPos = Input.mousePosition;
+        inputPos.z = camDistance;
+        Vector3 finalPos = Camera.main.ScreenToWorldPoint(inputPos);
+        //
+        rigBody.velocity = new Vector3(0.0f, 0.0f, 0.0f);
+        Vector3 newPos = rigBody.position;
+        newPos.x = mousePos.x;
+        newPos.y = 4.0f;
+        newPos.z = mousePos.z;
+        rigBody.position = newPos;
     }
 }
