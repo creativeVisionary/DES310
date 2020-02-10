7b52ef1d519cefe504350036b023019947450982
Mathew Brown <1700925@uad.ac.uk>
2020-02-10 13:49:46 +0000

    Randomised Baggage With Colour Coding and Models
    Randomised Baggage system implemented with both random colours and random selection from a set of hand picked models.

---
+++ Flockpit/Assets/Scripts/WorldScripts/RandomisedBaggageScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomisedBaggageScript : MonoBehaviour
+{
+    Material objmat;
+    Mesh objMesh;
+    public float maxRandRange = 100.0f;
+    //Reliant on minimum being 0.0f
+    float minRandRange = 0.0f;
+    public List<Color> colourList;
+    public Component[] renderList;
+    //Set in inspector using children of parent game object
+    public List<GameObject> modelList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RandMesh();
+        RandCol();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void RandCol()
+    {
+        int numOfVariations = colourList.Count;
+        float randValue = Random.Range(minRandRange, maxRandRange);
+        float rangeSegment = maxRandRange / numOfVariations;
+        for (int i = 0; i < numOfVariations; i++)
+        {
+            float lowVal = rangeSegment * i;
+            float highVal = rangeSegment * (i + 1);
+            if ((randValue <= highVal) && (randValue > lowVal))
+            {
+                int matNumber = GetComponentInChildren<Renderer>().materials.Length;
+                for (int l = 0;l < matNumber; l++)
+                {
+                    objmat = GetComponentInChildren<Renderer>().materials[l];
+                    objmat.color = colourList[i];
+                }
+            }
+        }
+    }
+
+    void RandMesh()
+    {
+        int numOfVariations = modelList.Count;
+        float randValue = Random.Range(minRandRange, maxRandRange);
+        float rangeSegment = maxRandRange / numOfVariations;
+        for (int i = 0; i < numOfVariations; i++)
+        {
+            float lowVal = rangeSegment * i;
+            float highVal = rangeSegment * (i + 1);
+            if ((randValue <= highVal) && (randValue > lowVal))
+            {
+                modelList[i].SetActive(true);
+            } else
+            {
+                modelList[i].SetActive(false);
+            }
+        }
+    }
+    
+}
