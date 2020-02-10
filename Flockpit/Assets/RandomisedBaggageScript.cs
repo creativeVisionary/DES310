79e57b96a97db750fdd734228e01eb6469a7b821
Mathew Brown <1700925@uad.ac.uk>
2020-02-10 11:34:30 +0000

    Randomised Baggage Started
    Simple randomisation on baggage items allowing for different colours to be applied dependent on a random chance.

---
+++ Flockpit/Assets/RandomisedBaggageScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomisedBaggageScript : MonoBehaviour
+{
+    Material objmat;
+    public float maxRandRange = 100.0f;
+    //Reliant on minimum being 0.0f
+    float minRandRange = 0.0f;
+    public List<Color> colourList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int numOfVariations = colourList.Count;
+        objmat = GetComponent<Renderer>().material;
+        objmat.color = Color.white;
+        float randValue = Random.Range(minRandRange, maxRandRange);
+        float rangeSegment = maxRandRange / numOfVariations;
+        for (int i = 0; i < numOfVariations; i++)
+        {
+            float lowVal = rangeSegment * i;
+            float highVal = rangeSegment * (i + 1);
+            if ((randValue <= highVal)&&(randValue > lowVal))
+            {
+                objmat.color = colourList[i];
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
