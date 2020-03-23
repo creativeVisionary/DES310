c26af2d9ada457c10c34d314cb7db5cd5ad36531
Claire <1602240@uad.ac.uk>
2020-03-23 16:17:09 +0000

    Camera Drag Hub World
    Click and drag to move camera

--- Flockpit/Assets/Scripts/DragCameraScript.cs
+++ Flockpit/Assets/Scripts/DragCameraScript.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 public class DragCameraScript : MonoBehaviour
 {
     [Range(1.0f,100.0f)]
-    public float maxSpeed = 20.0f;
-    public Vector3 cursorPosition;
-    Vector3 targetPos;
-    public float currentRotateAngle;
+    public float maxSpeed = 100.0f;
     [Range(1.0f, 360.0f)]
     public float speedVal = 0.0f;
     bool initialCatch = false;
     public Camera cam;
+
+    Vector3 oldPos;
+    Vector3 panOrigin;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,40 +21,48 @@ public class DragCameraScript : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetMouseButtonDown(0) && initialCatch == false) {
-            cursorPosition = Input.mousePosition;
-            initialCatch = true;
+        //tidy up speed value
+        if (speedVal > 360.0f)
+        {
+            speedVal = (speedVal - 360.0f);
         }
+        if (speedVal < 0.0f)
+        {
+            speedVal = (speedVal + 360.0f);
+        }
+
+        //set initial positions of camera and mouse
         if (Input.GetMouseButtonDown(0))
         {
-            speedVal += (cursorPosition - Input.mousePosition).magnitude;
-        } else if (Input.GetMouseButtonUp(0))
+            initialCatch = true;
+            oldPos = transform.position;
+            panOrigin = Camera.main.ScreenToViewportPoint(Input.mousePosition);//screen vector of mouse on click
+        }
+
+        //calculate update of the camera position in relation to mouse position change
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition) - panOrigin;//calculate difference between old and new mouse position
+            Vector3 newPos = oldPos + -pos; //add now position to old position
+            speedVal += newPos.x * maxSpeed;//update speed value with new position
+
+            //update positions
+            //If these are not here, the screen will scroll endlessly in current mouse position direction
+            oldPos = transform.position;
+            panOrigin = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        }
+
+        //end drag
+        if (Input.GetMouseButtonUp(0))
         {
             initialCatch = false;
         }
-        //this.gameObject.transform.eulerAngles = new Vector3(0.0f, 1.0f*speedVal, 0.0f);
-        //if (speedVal > 1.0f)
-        //{
-        //    speedVal--;
-        //    if (speedVal < 1.0f)
-        //    {
-        //        speedVal = 0.0f;
-        //    }
-        //}
-        //else if (speedVal < -1.0f)
-        //{
-        //    speedVal++;
-        //    if (speedVal > -1.0f)
-        //    {
-        //        speedVal = 0.0f;
-        //    }
-        //}
-
 
     }
 
     private void Update()
     {
+        //update the position of the camera
         this.gameObject.transform.eulerAngles = new Vector3(0.0f, 1.0f * speedVal, 0.0f);
     }
 }
