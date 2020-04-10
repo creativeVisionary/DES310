6d1698accd06ca2be7ee91a841fae3d9b3ff048f
Mathew Brown <1700925@uad.ac.uk>
2020-04-10 15:10:32 +0100

    Hub Implementation, Main Menu Alterations
    Hub Design from VisualTest branch merged and implemented into scene. Hub Game info UI for when a door is clicked implemented(Only on immigration Door). Menu Button Textures Set and arrangement altered.

---
+++ Flockpit/Assets/HubDoorScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HubDoorScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string gameTitle;
+    public string sceneName;
+    public GameObject gameUIPrefab;
+
+    private void OnMouseDown()
+    {
+
+      GameObject gameDetailsUI =  Instantiate(gameUIPrefab);
+        gameDetailsUI.GetComponent<GameInfoUIScript>().SetDetails(gameTitle, sceneName) ;
+    }
+}
