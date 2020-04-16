cc6f25251b6c1b3f1a0b93920602f2a2c66c5f6a
Mathew Brown <1700925@uad.ac.uk>
2020-04-16 14:50:40 +0100

    Lane System Implemented

---
+++ Flockpit/Assets/LaneScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaneScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.collider.gameObject.name.Contains("Passenger") && !Input.GetMouseButtonDown(0))
+    //    {
+    //        collision.gameObject.transform.position = new Vector3(this.transform.position.x, collision.gameObject.transform.position.y, collision.gameObject.transform.position.z);
+    //    }
+    //}
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.collider.gameObject.name.Contains("Passenger") && !Input.GetMouseButtonDown(0))
+        {
+            collision.gameObject.transform.position = new Vector3(this.transform.position.x, collision.gameObject.transform.position.y, collision.gameObject.transform.position.z);
+        }
+    }
+}
