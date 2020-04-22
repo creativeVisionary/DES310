83d44d23e358dacb93d3c37d6a409bb586d52aa4
Claire <1602240@uad.ac.uk>
2020-04-22 17:48:54 +0100

    Code cleanup for better passenger movement
    Passengers always snap to lanes correnctly

--- Flockpit/Assets/Scripts/Baggage/OnMouseDragScript.cs
+++ Flockpit/Assets/Scripts/Baggage/OnMouseDragScript.cs
@@ -27,7 +27,7 @@ public class OnMouseDragScript : MonoBehaviour
         if ((!score.gamePause)&&(score.gameStarted))
         {
             //move the passenger along the x axis
-            Vector3 newPos = new Vector3(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 21)).x, -1, gameObject.transform.position.z);
+            Vector3 newPos = new Vector3(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 21)).x, -0.4f, gameObject.transform.position.z) ;
             if ((newPos.x > levelBarriers[0].position.x ) && (newPos.x < levelBarriers[1].position.x) &&(passedGateLine == false))
             {
                 gameObject.transform.position = newPos;
