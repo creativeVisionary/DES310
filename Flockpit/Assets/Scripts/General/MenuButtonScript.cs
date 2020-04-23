cae420cd2355618f5fc33d0d8c21c74c5d3abaac
Mathew Brown <1700925@uad.ac.uk>
2020-04-23 14:08:48 +0100

    Script Placement & Temp Materials-Re-organisation Of Assets
    Scripts placed into proper folders and Temporary material files deleted.

---
+++ Flockpit/Assets/Scripts/General/MenuButtonScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MenuButtonScript : MonoBehaviour
+{
+
+    private void Start()
+    {
+    }
+
+    public void changeScene(string sceneName)
+    {
+        Debug.Log("Scene Changing To:" + sceneName);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+
+}
